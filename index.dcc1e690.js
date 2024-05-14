f49ed65f438efe8bc8dbc8cc35e5fd2c34280dc4
Vadim <volandemortuwka@gmail.com>
2024-05-14 11:24:21 +0300

    make build

---
+++ index.dcc1e690.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table").rows,l=0;l<e.length;l++){var r=e[l].cells[1].cloneNode(!0);e[l].insertBefore(r,e[l].lastElementChild)}
+//# sourceMappingURL=index.dcc1e690.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const table = document.querySelector("table");
+const rows = table.rows;
+for(let i = 0; i < rows.length; i++){
+    const clone = rows[i].cells[1].cloneNode(true);
+    rows[i].insertBefore(clone, rows[i].lastElementChild);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
